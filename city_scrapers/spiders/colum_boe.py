f38aa7079e79c072c1b3abc6059d152bf5986fd1
1992tw <47179662+1992tw@users.noreply.github.com>
2026-02-18 14:44:38 -0600

    Add title, description, classification

--- city_scrapers/spiders/colum_boe.py
+++ city_scrapers/spiders/colum_boe.py
@@ -1,3 +1,4 @@
+from urllib import response
 from city_scrapers_core.constants import NOT_CLASSIFIED
 from city_scrapers_core.items import Meeting
 from city_scrapers_core.spiders import CityScrapersSpider
@@ -30,19 +31,14 @@ class ColumBoeSpider(CityScrapersSpider):
 
         meetings = response.json()
 
-        for meeting in meetings:
-            meeting_id = meeting.get("unique")
-            print("Meeting:", meeting_id)
-
-            yield scrapy.Request(
-                url=self.detail_url,
-                method="POST",
-                body=f"current_committee_id={self.boarddocs_committee_id}&id={meeting_id}",
-                callback=self.parse,
-            )
-
-
-    
+        # for meeting in meetings:
+        meeting_id = meetings[0].get("unique")
+        yield scrapy.Request(
+            url=self.detail_url,
+            method="POST",
+            body=f"current_committee_id={self.boarddocs_committee_id}&id={meeting_id}",
+            callback=self.parse,
+        )
 
     def parse(self, response):
         """
@@ -51,40 +47,52 @@ class ColumBoeSpider(CityScrapersSpider):
         Change the `_parse_title`, `_parse_start`, etc methods to fit your scraping
         needs.
         """
-        meetings = response.json()
+        print("RESPONSE: ", response.text)
 
-        # print("Meetings:", meetings)
+        # for item in response.css(".meetings"):
+        #     meeting = Meeting(
+        #         title=self._parse_title(item),
+        #         description=self._parse_description(item),
+        #         classification=self._parse_classification(item),
+        #         start=self._parse_start(item),
+        #         end=self._parse_end(item),
+        #         all_day=self._parse_all_day(item),
+        #         time_notes=self._parse_time_notes(item),
+        #         location=self._parse_location(item),
+        #         links=self._parse_links(item),
+        #         source=self._parse_source(response),
+        #     )
 
-        #for item in response.css(".meetings"):
-            # meeting = Meeting(
-            #     title=self._parse_title(item),
-            #     description=self._parse_description(item),
-            #     classification=self._parse_classification(item),
-            #     start=self._parse_start(item),
-            #     end=self._parse_end(item),
-            #     all_day=self._parse_all_day(item),
-            #     time_notes=self._parse_time_notes(item),
-            #     location=self._parse_location(item),
-            #     links=self._parse_links(item),
-            #     source=self._parse_source(response),
-            # )
+        #     meeting["status"] = self._get_status(meeting)
+        #     meeting["id"] = self._get_id(meeting)
 
-            # meeting["status"] = self._get_status(meeting)
-            # meeting["id"] = self._get_id(meeting)
+        yield self._parse_title(response)
+        yield self._parse_description(response)
+        yield self._parse_classification(response)
 
-        yield None
+
 
     def _parse_title(self, item):
         """Parse or generate meeting title."""
-        return ""
+        title = item.css(".meeting-name::text").get()
+        print("TITLE >>>> : ", title)
+        return title
 
     def _parse_description(self, item):
         """Parse or generate meeting description."""
+        description= item.css(".meeting-description::text").getall()
+        print("DESCRIPTION >>>> : ", description)
         return ""
 
     def _parse_classification(self, item):
         """Parse or generate classification from allowed options."""
-        return NOT_CLASSIFIED
+        print("TITLE in classification>>>> : ", self._parse_title(item))
+        if "BOARD" in self._parse_title(item):
+            print("CLASSIFICATION >>>> : ", "Board")
+            return "Board"
+        else:
+            print("CLASSIFICATION >>>> : ", NOT_CLASSIFIED)
+            return NOT_CLASSIFIED
 
     def _parse_start(self, item):
         """Parse start datetime as a naive datetime object."""
@@ -116,3 +124,4 @@ class ColumBoeSpider(CityScrapersSpider):
     def _parse_source(self, response):
         """Parse or generate source."""
         return response.url
+
